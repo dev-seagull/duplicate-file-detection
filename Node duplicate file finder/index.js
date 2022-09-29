f50d8d9f2ec8c1a26136b418b80827e2e8b74acd
Kasra Alizadeh <k.alizadeh1997@gmail.com>
2022-09-29 18:54:38 +0330

    dynamic path input added to the code

--- Node duplicate file finder/index.js
+++ Node duplicate file finder/index.js
@@ -1,7 +1,18 @@
 const { findDuplicates } = require('duplicate-file-finder');
+const prompt = require('prompt-sync')();
 
-const sourcePath = 'C:/Users/User/Desktop/Data/SecondFile'; 
-const searchPaths = ['C:/Users/User/Desktop/Data/FirstFile','C:/Users/User/Desktop/Data/thirdFile']; 
+
+const sourcePath = prompt('Enter the sourcePath:');
+const numOfSearchPaths = Number(prompt('Enter the number of search path:'));
+const searchPaths = [];
+
+
+for(let num = 0; num < numOfSearchPaths; num++){
+    searchPaths[num] = prompt("Enter the search path: ")
+}
+
+ 
+//const searchPaths = ['C:/Users/User/Desktop/Data/FirstFile','C:/Users/User/Desktop/Data/thirdFile']; 
 const duplicates = findDuplicates({ sourcePath, searchPaths }).then(duplicates => {
     console.log(duplicates);
 });
