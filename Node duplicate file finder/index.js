d0530d05fd1219ce0a7e39b7008d0b5109513274
Kasra Alizadeh <k.alizadeh1997@gmail.com>
2022-09-29 19:28:05 +0330

    Output file writer added to the code(type of file is json)

--- Node duplicate file finder/index.js
+++ Node duplicate file finder/index.js
@@ -1,5 +1,6 @@
 const { findDuplicates } = require('duplicate-file-finder');
 const prompt = require('prompt-sync')();
+const fs = require('fs');
 
 
 const sourcePath = prompt('Enter the sourcePath:');
@@ -11,8 +12,17 @@ for(let num = 0; num < numOfSearchPaths; num++){
     searchPaths[num] = prompt("Enter the search path: ")
 }
 
- 
-//const searchPaths = ['C:/Users/User/Desktop/Data/FirstFile','C:/Users/User/Desktop/Data/thirdFile']; 
+  
 const duplicates = findDuplicates({ sourcePath, searchPaths }).then(duplicates => {
     console.log(duplicates);
+    duplicates = JSON.stringify(duplicates);
+    fs.writeFile("duplicatedFiles.json", duplicates, (err) => {
+        if (err) 
+        console.log("An error occured while writing JSON Object to File.");
+        else {
+        console.log("Duplicated files has been saved.");
+        } 
+      });
 });
+
+//C:/Users/User/Desktop/Data/FirstFile
